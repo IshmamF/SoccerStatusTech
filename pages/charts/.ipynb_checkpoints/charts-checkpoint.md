d9d7266e085773abb362458d86913f6cb9e140ff
muyuan2003 <70287030+muyuan2003@users.noreply.github.com>
2024-02-11 04:10:37 -0500

    fixed font in predictions

---
+++ pages/charts/.ipynb_checkpoints/charts-checkpoint.md
@@ -0,0 +1,8 @@
+<br/>
+<br/>
+<|part|render={showGraphs}|
+<center>
+<|{selected_team}|toggle|lov={values}|on_change=toggle_choice|>
+</center>
+<|{data}|chart|type=pie|values=Count|labels=Country|>
+|>
---
+++ pages/prediction/.ipynb_checkpoints/predictions-checkpoint.py
@@ -0,0 +1,14 @@
+from taipy.gui import Markdown
+
+showPred=False
+home_team_1 = "H1"
+home_goals_1 = '0'
+away_team_1 = "A1"
+away_goals_1 = '0'
+home_team_2 = "H2"
+home_goals_2 = '0'
+away_team_2 = "A2"
+away_goals_2 = '0'
+
+
+predictions_md = Markdown("predictions.md")
